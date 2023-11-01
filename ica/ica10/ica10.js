3854ca265ad80ed9b7d2fb43c86c38c071ee7606
rorycoflynn <143354491+rorycoflynn@users.noreply.github.com>
2023-10-31 19:26:36 -0600

    ica10

--- ica/ica10/ica10.js
+++ ica/ica10/ica10.js
@@ -3,7 +3,7 @@ let currentImage = 1;
 
 function changeImage() { //define the funciton
     currentImage = (currentImage % 6) + 1; //this will cycle thru the 6 pix. using modulo to get the position in the cycle and then +1 to move on from it
-    document.getElementById('kittyImage').src = `kitty${currentImage}.jpg`; //update src 
+    document.getElementById('kittyImage').src = `cat${currentImage}.jpg`; //update src 
     // referenced this link: https://stackoverflow.com/questions/7312553/change-image-source-with-javascript
 }
 
