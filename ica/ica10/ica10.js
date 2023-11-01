303e45fec864972cebed57f06930d960999cf010
rorycoflynn <143354491+rorycoflynn@users.noreply.github.com>
2023-10-31 19:15:05 -0600

    ica

---
+++ ica/ica10/ica10.js
@@ -0,0 +1,12 @@
+
+let currentImage = 1; 
+
+function changeImage() { //define the funciton
+    currentImage = (currentImage % 6) + 1; //this will cycle thru the 6 pix. using modulo to get the position in the cycle and then +1 to move on from it
+    document.getElementById('kittyImage').src = `kitty${currentImage}.jpg`; //update src 
+    // referenced this link: https://stackoverflow.com/questions/7312553/change-image-source-with-javascript
+}
+
+document.getElementById('kittyImage').addEventListener('click', changeImage); //this is the actual listener 
+
+ // referenced this link: https://stackoverflow.com/questions/7312553/change-image-source-with-javascript
