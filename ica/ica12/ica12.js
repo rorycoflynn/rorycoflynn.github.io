5030d3e48cb9fdb3e8352b413ff06c1942eb61d9
rorycoflynn <143354491+rorycoflynn@users.noreply.github.com>
2023-11-15 18:24:03 -0700

    fixed answer button ica12

--- ica/ica12/ica12.js
+++ ica/ica12/ica12.js
@@ -1,42 +1,41 @@
-const newBtn = document.querySelector('#js-new-quote').
-addEventListener('click', getQuote);
-const answerBtn = document.querySelector('#js-tweet').
-addEventListener('click', )
-let answer = ""
+const newBtn = document.querySelector('#js-new-quote').addEventListener('click', getQuote);
+const answerBtn = document.querySelector('#js-tweet').addEventListener('click', displayAnswer);
 
-const endpoint = 'https://trivia.cyberwisp.com/getrandomchristmasquestion'
+const questionTxt = document.querySelector('#js-quote-text');
+let answerTxt = document.querySelector('#js-answer-text');
+
+let answer = '';
+const endpoint = 'https://trivia.cyberwisp.com/getrandomchristmasquestion';
 
 async function getQuote() {
-    console.log('Test')
-
+   
     try {
-
         const response = await fetch(endpoint);
         if (!response.ok) {
             throw Error(response.statusText);
-        }
+    }
         const json = await response.json();
-        //console.log(json);
-        displayQuote(json['question'])
         
-        answer = json['answer']
+       
+        displayQuote(json['question']);
+        answer = json['answer'];
+        answerTxt.textContent = '';
     }
     catch(err) {
         console.log(err);
-        alert('Failed to fetch new quote')
+        alert('Failed to fetch new quote');
     }
 }
 
+// shows the question
 function displayQuote(question) {
-    const questionTxt = document.querySelector
-    ('#js-quote-text');
     questionTxt.textContent = question;
 }
 
-function displayAnswer(answer) {
-    const answerTxt = document.querySelector
-    ('#js-answer-text');
-    answerTxt.textContent = '';
+// this shows the answer
+function displayAnswer() {
+    answerTxt.textContent = answer;
 }
 
+// we run getQuote once when the script first loaded
 getQuote();
