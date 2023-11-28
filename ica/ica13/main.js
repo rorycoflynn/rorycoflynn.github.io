2c55612d5e8e380832c251619379462d65a33067
rorycoflynn <143354491+rorycoflynn@users.noreply.github.com>
2023-11-28 10:20:36 -0700

    135 balls

--- ica/ica13/main.js
+++ ica/ica13/main.js
@@ -79,7 +79,7 @@ class Ball {
 const balls = [];
 
 //while loop uses list of balls (as long as there are less than 26 obj in list, the loop will continue)
-while (balls.length < 25) {
+while (balls.length < 135) {
   const size = random(10, 20);
   const ball = new Ball(
     // ball position always drawn at least one ball width
